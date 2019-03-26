8946ce30be2c4b35f96897fe90ffab5a97acb5fa
lujunhui <lujunhui_c@163.com>
2019-03-26 22:18:03 +0800

    修改教学中心

--- 教学质量监控中心/js/index.js
+++ 教学质量监控中心/js/index.js
@@ -11,7 +11,7 @@ function nextpeo() {
     all[i].style.zIndex="-1";
     all[i].style.opacity="1";
     buttons[i].className="but on";
-    if(i == all.length-1)
+    if(i === all.length-1)
         i=0;
     else
         i++;
@@ -27,3 +27,36 @@ function animated(){
     },0);
 }
 animated();
+
+
+//轮播图
+var link_text=document.getElementsByClassName("link_text");
+
+var arrlink_text=["link_text1","link_text2","link_text3"];
+var ha=1;
+function nextpeo_link_text() {
+    console.log("1111");
+    arrlink_text.unshift(arrlink_text[2]);
+    arrlink_text.pop();
+    for(let j=0;j<link_text.length;j++){
+        link_text[j].className="link_text"+ " " +arrlink_text[j];
+    }
+
+    if(ha === link_text.length-1)
+        ha=0;
+    else
+        ha++;
+}
+var time_link_text=null;
+function animated_link_text(){
+    setTimeout(function () {
+        nextpeo_link_text();
+        time_link_text=setInterval(function(){
+            nextpeo_link_text();
+        },5000);
+
+    },0);
+}
+animated_link_text();
+
+
